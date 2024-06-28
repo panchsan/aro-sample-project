7b9d5f01259f5a580c5db16ac7bbd9a906b13335
v-santanup <v-santanup@SANTANU>
2024-06-28 15:17:08 +0530

    Add project files.

---
+++ aro-sample-project/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using aro_sample_project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace aro_sample_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ aro-sample-project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aro_sample_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

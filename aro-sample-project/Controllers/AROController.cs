6e861f0ef1a3bc833f01899b110bdc226ac516e6
santanu299118@outlook.com <santanu299118@outlook.com>
2024-07-17 13:49:24 +0530

    Updated the application

---
+++ aro-sample-project/Controllers/AROController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aro_sample_project.Controllers
+{
+    public class AROController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
